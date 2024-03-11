c529dd94c2c50d9b0755c9b961072569e81fb86b
eitanby <eitanby@icloud.com>
2024-03-11 10:02:38 +0200

    cleanup logic

--- internal/do-format/formatter.go
+++
@@ -1,9 +0,0 @@
-// Package format provides various utilities to
-// make it easy to convert values to strings
-package format
-
-import "fmt"
-
-func Number(num int) string {
-	return fmt.Sprintf("the number is %d", num)
-}
--- main.go
+++ main.go
@@ -2,12 +2,8 @@ package main
 
 import (
 	"fmt"
-	"github.com/eitanby/app-vision/internal/do-format"
-	"github.com/eitanby/app-vision/internal/math"
 )
 
 func main() {
-	num := math.Double(4)
-	output := format.Number(num)
-	fmt.Println(output)
+	fmt.Println("hello world")
 }
