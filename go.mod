cbce26577b97ec555fcc638b87d4dc0cbe568e57
eitanby <eitanby@icloud.com>
2024-03-11 10:18:14 +0200

    update package

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/eitanby/app-vision
 
 go 1.22.1
+
+require github.com/eitanby/app-util v0.0.1
--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	apputil "github.com/eitanby/app-util"
 )
 
 func main() {
-	fmt.Println("hello world")
+	result := apputil.SpecialAdd(5, 5)
+	fmt.Printf("result is %d", result)
 }
